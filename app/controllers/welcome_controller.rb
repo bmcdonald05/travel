051bf4c64175426a7b9b19439d3f38795345ba82
Brian McDonald <bmcdonald05@gmail.com>
2016-04-12 11:27:30 -0400

    After class

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,13 @@ class WelcomeController < ApplicationController
   	@homeland = 'USA'
   	@countries = %w[Chile Boliva Croatia]
   	@places = ["winter.jpg", "tropical.jpg", "chile.jpg", "croatia.jpg"]
+  	@united_states = {"Capital City" => "Washington, D.C.", "Favorite City" => "Raleigh", "Favorite State" => "Colorado", 
+  		"Flag Colors" => ["Red", "White", "Blue"]}
   end
-
   def about
   	@color = params[:color]
   	@size = params[:size].to_i
+  end
+  def contact
   end
 end
